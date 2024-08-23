fda3228935a471ffe224d0523e8b16238162d15d
Wesley Springer <wspringe@outlook.com>
2024-08-22 22:43:45 -0700

    feat: add apex language support

---
+++ languages/apex/indents.scm
@@ -0,0 +1,16 @@
+[
+    (assignment_expression)
+    (binary_expression)
+    (instanceof_expression)
+    (ternary_expression)
+    (update_expression)
+    (primary_expression)
+    (unary_expression)
+    (cast_expression)
+    (switch_expression)
+] @indent
+
+(_ "[" "]" @end) @indent
+(_ "<" ">" @end) @indent
+(_ "{" "}" @end) @indent
+(_ "(" ")" @end) @indent
